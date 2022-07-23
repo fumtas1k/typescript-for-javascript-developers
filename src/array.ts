c3e76df9e649302b06b2330786a5383b0588f322
fumtas1k <fumta01a7@gmail.com>
2022-07-23 15:44:55 +0900

    add:array

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [4, 5, 6];
+let strings: string[] = ["Tokyo", "JavaScript", "CoffeeScript"];
+let nijigenHairetsu: number[][] = [[50, 100], [150, 300]];
+let hairetsu: (number | boolean | string)[] = [1, false, 'Japan'];
+
+console.log({ numbers });
+console.log({ numbers2 });
+console.log({ strings });
+console.log({ nijigenHairetsu });
+console.log({ hairetsu });
