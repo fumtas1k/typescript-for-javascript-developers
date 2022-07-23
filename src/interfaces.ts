30f69417309c263cbf9bbae7ea9cb4f0b3195d7b
fumtas1k <fumta01a7@gmail.com>
2022-07-23 16:56:59 +0900

    add:interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,19 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+}
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+
+let object: ObjectInterface = {
+  name: "Ham-san",
+  age: 43
+};
+
+console.log({ object });
