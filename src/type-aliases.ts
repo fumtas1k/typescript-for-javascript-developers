0d62a0f5077a70a993bfcc45f8501e77c2ff74bd
fumtas1k <fumta01a7@gmail.com>
2022-07-23 16:51:54 +0900

    type-aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,26 @@
+export {};
+
+type Mojiretsu = string;
+
+let str: Mojiretsu = "文字列";
+
+const example1 = {
+  name: "Ham",
+  age: 43,
+}
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const example2: Profile = {
+  name: "Ham",
+  age: 43,
+}
+
+type Profile2 = typeof example1;
+
+
+
+console.log(str);
