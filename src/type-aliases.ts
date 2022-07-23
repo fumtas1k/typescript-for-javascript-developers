c721478ac78cf51c08384847e5651ce7edb2baed
fumtas1k <fumta01a7@gmail.com>
2022-07-23 16:52:50 +0900

    add:type-aliases

--- src/type-aliases.ts
+++ src/type-aliases.ts
@@ -7,7 +7,7 @@ let str: Mojiretsu = "文字列";
 const example1 = {
   name: "Ham",
   age: 43,
-}
+};
 
 type Profile = {
   name: string;
@@ -17,10 +17,9 @@ type Profile = {
 const example2: Profile = {
   name: "Ham",
   age: 43,
-}
+};
 
 type Profile2 = typeof example1;
-
 
 
 console.log(str);
